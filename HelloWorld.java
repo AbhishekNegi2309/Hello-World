6a39d31153fc622749084ce0b61fd820d9465601
Avneet Pandey <59452209+avneetpandey82@users.noreply.github.com>
2020-10-09 14:36:06 +0530

    Create HelloWorld.java
    In Java

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+//This is to just demonstrate the basic program in different language or we can say first program in the life of programmer
+class Main{
+      public static void main(String[] args){
+        System.out.println("Hello World");
+      }
+}
